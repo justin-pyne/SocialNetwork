bbecf27b5699e38880578ca5f3944314ebaa4391
justin-pyne <jpyne.justin@gmail.com>
2024-09-25 12:04:27 -0700

    init jsonprocessor, profile constructor/getters/setters

--- src/main/java/app/Profile.java
+++ src/main/java/app/Profile.java
@@ -8,5 +8,24 @@ abstract public class Profile {
     private String image;
     private List<Post> posts;
 
+    public Profile(String name, int year, String image, List<Post> posts) {
+        this.name = name;
+        this.year = year;
+        this.image = image;
+        this.posts = posts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
 
 }
